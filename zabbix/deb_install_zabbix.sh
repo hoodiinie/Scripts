aa61c33fae4fbaa00aa8a6c04f5f42ffc6b25aba
hoodiinie <whitehoodieni@outlook.fr>
2022-03-24 08:25:13 +0100

    Deplacer

---
+++ zabbix/deb_install_zabbix.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+clear
+tput setaf 7; read -p "Entrez le mot de passe pour la base de données Zabbix : " ZABBIX_DB_USER_PASSWORD
+tput setaf 2; echo ""
+
+addr_ip=$(hostname -I)
+
+
+function install_docker ()
+{
+    tput setaf 6; echo ""
+
+    apt-get install -y apt-transport-https ca-certificates curl gnupg2 software-properties-common
+    curl -fsSL https://download.docker.com/linux/$(. /etc/os-release; echo "$ID")/gpg | apt-key add -
+    add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/$(. /etc/os-release; echo "$ID") $(lsb_release -cs) stable"
+    apt-get update
+    apt-get -y install docker-ce docker-compose
+    systemctl enable docker
+    systemctl start docker
+
+    tput setaf 7; echo ""
+}
+
+function data ()
+{
+        if [[ ! -e /apps ]]
+        then
+                mkdir /apps
+
+                if [[ ! -e /apps/zabbix ]]
+                then
+                    mkdir /apps/zabbix
+                    mkdir /apps/zabbix/db
+                    mkdir /apps/zabbix/srv
+                    mkdir /apps/zabbix/agent
+                fi
+        fi
+}
+
+cp docker-compose.yml docker-compose.yml.bck
+
+# Modification et lancement du docker-compose.yml
+for file in ~/scripts/docker-compose.yml
+do
+  echo "Traitement de $file ..."
+  sed -i -e "s/zabbix-bdd-password/$ZABBIX_DB_USER_PASSWORD/g" "$file"
+done
+
+data
+install_docker
+docker-compose up -d
+
+rm docker-compose.yml
+mv docker-compose.yml.bck docker-compose.yml
+
+tput bold; tput setaf 7; echo "LISTES DES CONTAINERS EN COURS : "
+tput setaf 3; echo ""
+docker container ls
+echo ""
+tput setaf 7; echo "-------------------------------------------------"
+tput setaf 7; echo ""
+tput setaf 7; echo "   Adresse du serveur Zabbix : http://$addr_ip:8090/     "
+tput setaf 7; echo "         ID : Admin / MDP : zabbix             "
+tput setaf 7; echo ""
+tput setaf 7; echo "-------------------------------------------------"
+tput setaf 2; echo ""
