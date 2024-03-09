0eb6d5e2dba68a4f7bc87bf6c7cc578a6d182082
hoodiinie <whitehoodieni@outlook.fr>
2024-03-09 21:10:53 +0100

    3

--- Debian/zabbix_install.sh
+++ Debian/zabbix_install.sh
@@ -10,54 +10,78 @@ PASS=$1
 WEB=$2
 
 
-##VERIF
+verif_script()
+{
+	if [ $QUI = "root" ]
+	then
+		echo "ERREUR : Veuillez exécuter le script avec un utilisateur présent dans le fichier /etc/sudoers"
+		exit
+	fi
 
-if [ $QUI = "root" ]
-then
-	echo "ERREUR : Veuillez exécuter le script avec un utilisateur présent dans le fichier /etc/sudoers"
-	exit
-fi
+	if [[ -z $PASS ]]
+	then
+		echo "ERREUR : Veuillez indiquer le mot de passe de la base de données"	
+		exit
+	fi
 
-if [[ -z $PASS ]]
-then
-	echo "ERREUR : Veuillez indiquer le mot de passe de la base de données"	
-	exit
-fi
-
-if [[ -z $WEB ]]
-then
-	echo "ERREUR : Veuillez indiquer l'adresse IP ou le nom de domaine de l'interface Web"
-	exit
-fi
-
+	if [[ -z $WEB ]]
+	then
+		echo "ERREUR : Veuillez indiquer l'adresse IP ou le nom de domaine de l'interface Web"
+		exit
+	fi
+}
 
 ##START
 
 read -p "Quelle version de Zabbix souhaitez-vous installer ? " VERSIONZBX
 
-cd $WORKSPACE
+sources_download()
+{
+	cd $WORKSPACE
+	wget https://repo.zabbix.com/zabbix/"$VERSIONZBX"/debian/pool/main/z/zabbix-release/zabbix-release_"$VERSIONZBX"-1+debian"$VERSIONDEB"_all.deb
+	sudo dpkg -i zabbix-release_"$VERSIONZBX"-1+debian"$VERSIONDEB"_all.deb
+	sudo apt update
+}
 
-wget https://repo.zabbix.com/zabbix/"$VERSIONZBX"/debian/pool/main/z/zabbix-release/zabbix-release_"$VERSIONZBX"-1+debian"$VERSIONDEB"_all.deb
-sudo dpkg -i zabbix-release_"$VERSIONZBX"-1+debian"$VERSIONDEB"_all.deb
-sudo apt update
+packages_installation()
+{
+	sudo apt install postgresql
+	sudo apt install zabbix-server-pgsql zabbix-frontend-php php8.2-pgsql zabbix-nginx-conf zabbix-sql-scripts zabbix-agent
+}
 
-sudo apt install postgresql
-sudo apt install zabbix-server-pgsql zabbix-frontend-php php8.2-pgsql zabbix-nginx-conf zabbix-sql-scripts zabbix-agent
+setup_database()
+{
+	sudo -u postgres createuser --pwprompt zabbix
+	sudo -u postgres createdb -O zabbix zabbix
+	zcat /usr/share/zabbix-sql-scripts/postgresql/server.sql.gz | sudo -u zabbix psql zabbix
+}
 
-sudo -u postgres createuser --pwprompt zabbix
-sudo -u postgres createdb -O zabbix zabbix
+setup_conf()
+{
+	sudo sed -i 's/# DBPassword=/DBPassword='$PASS'/g' /etc/zabbix/zabbix_server.conf
+	sudo sed -i 's/#        listen          8080;/	listen 8080;/g' /etc/zabbix/nginx.conf
+	sudo sed -i 's/#        server_name     example.com;/	server_name '$WEB';/g' /etc/zabbix/nginx.conf
+}
 
-zcat /usr/share/zabbix-sql-scripts/postgresql/server.sql.gz | sudo -u zabbix psql zabbix
+setup_services()
+{
+	sudo systemctl restart zabbix-server zabbix-agent nginx php8.2-fpm
+	sudo systemctl enable zabbix-server zabbix-agent nginx php8.2-fpmtput setaf 
+}
 
-sudo sed -i 's/# DBPassword=/DBPassword='$PASS'/g' /etc/zabbix/zabbix_server.conf
-sudo sed -i 's/#        listen          8080;/	listen 8080;/g' /etc/zabbix/nginx.conf
-sudo sed -i 's/#        server_name     example.com;/	server_name '$WEB';/g' /etc/zabbix/nginx.conf
-
-sudo systemctl restart zabbix-server zabbix-agent nginx php8.2-fpm
-sudo systemctl enable zabbix-server zabbix-agent nginx php8.2-fpmtput setaf 
-
+main()
+{
+	verif_script
+	sources_download
+	packages_installation
+	setup_database
+	setup_conf
+	setup_services
+}
 
 ##END
+
+main
 
 tput setaf 1; echo "--------------------------------------------------------------------------------------------------"
 tput bold; tput setaf 6; echo "                                                                                       "
