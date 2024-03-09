32d6ea3222ecc4f23bfd7e26ceccfdb0568aff62
hoodiinie <whitehoodieni@outlook.fr>
2024-03-09 16:56:21 +0100

    Première version

---
+++ Debian/docker.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#INSTALL DOCKER
+
+apt-get install -y apt-transport-https ca-certificates gnupg2 software-properties-common
+curl -fsSL https://download.docker.com/linux/$(. /etc/os-release; echo "$ID")/gpg | apt-key add -
+add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/$(. /etc/os-release; echo "$ID") $(lsb_release -cs) stable"
+apt-get update
+apt-get -y install docker-ce docker-compose
+systemctl enable docker
+systemctl start docker
+
