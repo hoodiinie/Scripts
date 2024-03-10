11f3fa22fee5f3dabc04016f8bfecad8cc5caea1
hoodiinie <whitehoodieni@outlook.fr>
2024-03-10 10:52:22 +0100

    Script pour Debian

--- Debian/zabbix_install.sh
+++ Debian/zabbix_install.sh
@@ -14,7 +14,7 @@ verif_script()
 		exit
 	fi
 
-	if [[ -z $OPT_1 ]] || [[ $OPT_1 -ne "-z" ]]
+	if [[ -z $OPT_1 ]] || [[ $OPT_1 != "-z" ]]
 	then
 		echo "ERREUR : Veuillez indiquer l'option -z suivi du numéro de version de Zabbix (Exemple : 6.4)"
 		exit
@@ -26,7 +26,7 @@ verif_script()
 		exit
 	fi
 
-	if [[ -z $OPT_2 ]] || [[ $OPT_2 -ne "-p" ]]
+	if [[ -z $OPT_2 ]] || [[ $OPT_2 != "-p" ]]
 	then
 		echo "ERREUR : Veuillez indiquer l'option -p suivi du mot de passe de la base de données"
 		exit
@@ -38,12 +38,12 @@ verif_script()
 		exit
 	fi
 
-	if [[ -z $OPT_3 ]] || [[ $OPT_3 -ne "-w" ]]
+	if [[ -z $OPT_3 ]] || [[ $OPT_3 != "-w" ]]
 	then
 		echo "ERREUR : Veuillez indiquer l'option -w suivi de l'adresse IP ou le nom de domaine de l'interface Web"
 		exit
 	fi
-	
+
 	if [[ -z $WEB ]]
 	then
 		echo "ERREUR : Veuillez indiquer l'adresse IP ou le nom de domaine de l'interface Web"
