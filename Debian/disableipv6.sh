6a0c1478f32c827345143da6ee0e5f978918c197
hoodiinie <whitehoodieni@outlook.fr>
2024-03-15 13:58:52 +0100

    Script pour Debian

---
+++ Debian/disableipv6.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo -e "net.ipv6.conf.all.disable_ipv6 = 1
+net.ipv6.conf.all.autoconf = 0
+net.ipv6.conf.default.disable_ipv6 = 1
+net.ipv6.conf.default.autoconf = 0" >> /etc/sysctl.conf
+
+sysctl -p
+
+service networking restart
