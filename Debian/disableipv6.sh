0b84f3fbeccbad925af4447aaac15e4dd27c0184
hoodiinie <whitehoodieni@outlook.fr>
2024-03-15 14:08:32 +0100

    Script pour Debian

--- Debian/disableipv6.sh
+++ Debian/disableipv6.sh
@@ -1,10 +1,55 @@
 #!/bin/bash
 
-echo -e "net.ipv6.conf.all.disable_ipv6 = 1
-net.ipv6.conf.all.autoconf = 0
-net.ipv6.conf.default.disable_ipv6 = 1
-net.ipv6.conf.default.autoconf = 0" >> /etc/sysctl.conf
+WORKSPACE="/etc/sysctl.conf"
+QUI=$(whoami)
 
-sysctl -p
+verif_script()
+{
+	if [ "$QUI" != "root" ]
+	then
+		echo "ERREUR : Veuillez exécuter le script en mode root"
+		exit
+	fi
+}
 
-service networking restart
+disable_ipv6()
+{
+    TEXT="net.ipv6.conf.all.disable_ipv6 = 1"
+    IPV6=$(grep "$TEXT" $WORKSPACE)
+    if [ "$IPV6" != "$TEXT" ]
+    then
+        echo "$TEXT" > $WORKSPACE
+    fi
+
+    TEXT="net.ipv6.conf.all.autoconf = 0"
+    IPV6=$(grep "$TEXT" $WORKSPACE)
+    if [ "$IPV6" != "$TEXT" ]
+    then
+        echo "$TEXT" > $WORKSPACE
+    fi
+
+    TEXT="net.ipv6.conf.default.disable_ipv6 = 1"
+    IPV6=$(grep "$TEXT" $WORKSPACE)
+    if [ "$IPV6" != "$TEXT" ]
+    then
+        echo "$TEXT" > $WORKSPACE
+    fi
+
+    TEXT="net.ipv6.conf.default.autoconf = 0"
+    IPV6=$(grep "$TEXT" $WORKSPACE)
+    if [ "$IPV6" != "$TEXT" ]
+    then
+        echo "$TEXT" > $WORKSPACE
+    fi
+
+    service networking restart
+}
+
+main()
+{
+    verif_script
+    disable_ipv6
+    sysctl -p
+}
+
+main
